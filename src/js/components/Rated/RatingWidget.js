0d2783b85c72a10d96ab0e0078412572ed9271cd
saradonin <m.jemieljanczuk@gmail.com>
2024-02-26 20:38:01 +0100

    rating update fixed

--- src/js/components/Rated/RatingWidget.js
+++ src/js/components/Rated/RatingWidget.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { addToRated, removeFromWatchlist } from "../../redux/actions";
+import { addToRated, removeFromWatchlist, updateRating } from "../../redux/actions";
 import { Rating } from "@mui/material";
 
-export default function RatingWidget({movie}) {
+export default function RatingWidget({ movie }) {
     const [value, setValue] = useState(movie.userRating ? movie.userRating : 0)
     const dispatch = useDispatch()
 
     const handleRating = (e, newValue) => {
         e.preventDefault()
-        setValue(newValue)
-        dispatch(addToRated(movie, newValue))
-        dispatch(removeFromWatchlist(movie))
-    }
+        if (value === 0) {
+            setValue(newValue)
+            dispatch(addToRated(movie, newValue))
+            dispatch(removeFromWatchlist(movie))
+        } else {
+            setValue(newValue)
+            dispatch(updateRating(movie, newValue))
+        }
 
+    }
 
     return (
         <Rating
