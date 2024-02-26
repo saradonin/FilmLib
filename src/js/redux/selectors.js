ff7258c3bcbc3c56ef50de709b8fb0309fbffbcd
saradonin <m.jemieljanczuk@gmail.com>
2024-02-26 22:26:33 +0100

    selectors simplified

--- src/js/redux/selectors.js
+++ src/js/redux/selectors.js
@@ -1,68 +1,37 @@
 export const searchSelector = state => state.search
 
-export const watchlistSelector = (state, orderBy) => {
-    let watchlist = state.watchlist
-
+const sortByOrder = (array, orderBy) => {
+    
     const getYear = (yearAsString) => {
-        return parseInt(yearAsString.substring(0,4))
+        return parseInt(yearAsString.substring(0, 4))
     }
 
     switch (orderBy) {
         case 'titleAsc':
-            watchlist = watchlist.slice().sort((a, b) => a.Title.localeCompare(b.Title))
-            break
+            return array.slice().sort((a, b) => a.Title.localeCompare(b.Title))
         case 'titleDesc':
-            watchlist = watchlist.slice().sort((a, b) => b.Title.localeCompare(a.Title))
-            break
+            return array.slice().sort((a, b) => b.Title.localeCompare(a.Title))
         case 'ratingHigh':
-            watchlist = watchlist.slice().sort((a, b) => (b.userRating || 0) - (a.userRating || 0))
-            break
+            return array.slice().sort((a, b) => (b.userRating || 0) - (a.userRating || 0))
         case 'ratingLow':
-            watchlist = watchlist.slice().sort((a, b) => (a.userRating || 0) - (b.userRating || 0))
-            break
+            return array.slice().sort((a, b) => (a.userRating || 0) - (b.userRating || 0))
         case 'yearNew':
-            watchlist = watchlist.slice().sort((a, b) => getYear(b.Year) - getYear(a.Year))
-            break
+            return array.slice().sort((a, b) => getYear(b.Year) - getYear(a.Year))
         case 'yearOld':
-            watchlist = watchlist.slice().sort((a, b) => getYear(a.Year) - getYear(b.Year))
-            break
+            return array.slice().sort((a, b) => getYear(a.Year) - getYear(b.Year))
         default:
-            break
+            return array
     }
-    return watchlist
+}
+
+export const watchlistSelector = (state, orderBy) => {
+    return sortByOrder(state.watchlist, orderBy)
 }
 
 export const ratedSelector = (state, orderBy) => {
-    let rated = state.rated
-
-    const getYear = (yearAsString) => {
-        return parseInt(yearAsString.substring(0,4))
-    }
-
-    switch (orderBy) {
-        case 'titleAsc':
-            rated = rated.slice().sort((a, b) => a.Title.localeCompare(b.Title))
-            break
-        case 'titleDesc':
-            rated = rated.slice().sort((a, b) => b.Title.localeCompare(a.Title))
-            break
-        case 'ratingHigh':
-            rated = rated.slice().sort((a, b) => b.userRating - a.userRating)
-            break
-        case 'ratingLow':
-            rated = rated.slice().sort((a, b) => a.userRating - b.userRating)
-            break
-        case 'yearNew':
-            rated = rated.slice().sort((a, b) => getYear(b.Year) - getYear(a.Year))
-            break
-        case 'yearOld':
-            rated = rated.slice().sort((a, b) => getYear(a.Year) - getYear(b.Year))
-            break
-        default:
-            break
-    }
-    return rated
+    return sortByOrder(state.rated, orderBy)
 }
+
 
 
 
