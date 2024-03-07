3473af5f8d58d060507d6a2a8fec413d814f6d25
saradonin <m.jemieljanczuk@gmail.com>
2024-03-07 22:43:31 +0100

    movie details reworked from list to table

--- src/js/components/Movie/MovieDetails.js
+++ src/js/components/Movie/MovieDetails.js
@@ -1,59 +1,66 @@
 import React from "react";
-import { List, ListItem, ListItemText, Card, CardActions, CardContent, CardMedia, Typography, Box, Grid } from "@mui/material";
+import { Card, CardActions, CardMedia, Typography, Box, Table, TableBody, TableRow, TableCell } from "@mui/material";
 import RatingWidget from "../Rated/RatingWidget";
 import WatchListButtons from "../Watchlist/WatchListButtons";
 import { NO_IMAGE_URL } from "./constants";
 
 
 export default function MovieDetails({ movie, isMovieInWatchlist }) {
+
     return (
-            <Card sx={{ display: 'flex', flexDirection: 'column', maxHeight: 600, padding: 2 , margin: 3}}>
+        <Card sx={{ display: 'flex', flexDirection: 'column', maxHeight: 600, padding: 2, margin: 3 }}>
+            <Box>
+                <Typography gutterBottom variant="h4" component="div">
+                    {movie.Title}
+                </Typography>
+            </Box>
+
+            <Box sx={{ display: 'flex', flexDirection: 'row', }}>
                 <Box>
-                    <Typography gutterBottom variant="h4" component="div">
-                        {movie.Title}
-                    </Typography>
+                    <CardMedia
+                        component="img"
+                        sx={{ width: 240, height: 360 }}
+                        image={movie.Poster !== "N/A" ? movie.Poster : NO_IMAGE_URL}
+                        alt={`${movie.Title}`}
+                    />
                 </Box>
 
-                <Box sx={{display: 'flex', flexDirection: 'row', }}>
-                    <Box>
-                        <CardMedia
-                            component="img"
-                            sx={{ width: 240, height: 360 }}
-                            image={movie.Poster !== "N/A" ? movie.Poster : NO_IMAGE_URL}
-                            alt={`${movie.Title}`}
-                        />
-                    </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
 
-                    <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-                        <CardContent>
-                            <List>
-                                <ListItem>
-                                    <ListItemText primary={`Year: ${movie.Year}`} />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemText primary={`Genre: ${movie.Genre}`} />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemText primary={`Director: ${movie.Director}`} />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemText primary={`Writer: ${movie.Writer}`} />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemText primary={`Cast: ${movie.Actors}`} />
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemText primary="Plot: " secondary={movie.Plot} />
-                                </ListItem>
-                            </List>
-                        </CardContent>
-
-                        <CardActions sx={{ display: 'flex', justifyContent: 'end' }}>
-                            <RatingWidget movie={movie} />
-                            <WatchListButtons movie={movie} isMovieInWatchlist={isMovieInWatchlist} />
-                        </CardActions>
-                    </Box>
+                    <Table sx={{ minWidth: 360 }} aria-label="simple table">
+                        <TableBody>
+                            <TableRow>
+                                <TableCell align="left">Year:</TableCell>
+                                <TableCell align="left">{movie.Year}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Genre:</TableCell>
+                                <TableCell align="left">{movie.Genre}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Director:</TableCell>
+                                <TableCell align="left">{movie.Director}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Writer:</TableCell>
+                                <TableCell align="left">{movie.Writer}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Cast:</TableCell>
+                                <TableCell align="left">{movie.Actors}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Plot:</TableCell>
+                                <TableCell align="left">{movie.Plot}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                    <CardActions sx={{ display: 'flex', justifyContent: 'end' }}>
+                        <RatingWidget movie={movie} />
+                        <WatchListButtons movie={movie} isMovieInWatchlist={isMovieInWatchlist} />
+                    </CardActions>
                 </Box>
-            </Card>
+            </Box>
+        </Card>
     )
 }
